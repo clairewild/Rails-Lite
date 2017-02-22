288f4d66b9163be4640f23e6e7c00440156b2473
Claire Wild <claire.wild15@gmail.com>
2017-02-22 12:30:47 -0800

    Initial commit

---
+++ lib/controller_base.rb
@@ -0,0 +1,71 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'active_support/inflector'
+require 'erb'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+    @params = {}
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    if already_built_response?
+      raise "Double render error"
+    else
+      @already_built_response = true
+      @res['Location'] = url
+      @res.status = 302
+      @session.store_session(@res)
+    end
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    if already_built_response?
+      raise "Double render error"
+    else
+      @already_built_response = true
+      @res.write(content)
+      @res['Content-Type'] = content_type
+      @session.store_session(@res)
+    end
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    controller_name = self.class.to_s.underscore
+    path = "views/#{controller_name}/#{template_name}.html.erb"
+    contents = File.read(path)
+    template = ERB.new(contents)
+    render_content(template.result(binding), "text/html")
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(@req)
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+    self.send(name)
+    unless @already_built_response
+      render(name.to_s)
+    end
+  end
+end
